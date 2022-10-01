320cdfc79b88664947f146e67adb440bc3147c90
Scott-Dyer <113874294+Scott-Dyer@users.noreply.github.com>
2022-10-01 19:29:32 +1000

    Update Confidence.py

--- Confidence.py
+++ Confidence.py
@@ -10,10 +10,11 @@ from pprint import pprint
 def importance(confidence):
     network_name = "network_info/4932_protein_links_v11_5.txt"
     G = func.remove_threshold(network_name, confidence)
-
+    
     essential_proteins = "network_info/essential_proteins.csv"
     G = func.remove_essential(G, essential_proteins)
 
+    adj_matrix = nx.adjacency_matrix(G) 
     protein_hash = {}
     for index, node in enumerate(G.nodes):
         protein_hash[index] = node
